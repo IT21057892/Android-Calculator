095acba5392afa705820419954cab1c842c559dd
IT21057892 <99260819+IT21057892@users.noreply.github.com>
2023-03-11 09:59:44 +0530

    Initial Commit

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.DecimalFormat
 
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,92 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        button_clear.setOnClickListener {
+            input.text = ""
+            output.text = ""
+        }
+
+        button_bracket_left.setOnClickListener {
+            input.text = addToInputText("(")
+        }
+        button_bracket_right.setOnClickListener {
+            input.text = addToInputText(")")
+        }
+        button_0.setOnClickListener {
+            input.text = addToInputText("0")
+        }
+        button_1.setOnClickListener {
+            input.text = addToInputText("1")
+        }
+        button_2.setOnClickListener {
+            input.text = addToInputText("2")
+        }
+        button_3.setOnClickListener {
+            input.text = addToInputText("3")
+        }
+        button_4.setOnClickListener {
+            input.text = addToInputText("4")
+        }
+        button_5.setOnClickListener {
+            input.text = addToInputText("5")
+        }
+        button_6.setOnClickListener {
+            input.text = addToInputText("6")
+        }
+        button_7.setOnClickListener {
+            input.text = addToInputText("7")
+        }
+        button_8.setOnClickListener {
+            input.text = addToInputText("8")
+        }
+        button_9.setOnClickListener {
+            input.text = addToInputText("9")
+        }
+        button_dot.setOnClickListener {
+            input.text = addToInputText(".")
+        }
+        button_division.setOnClickListener {
+            input.text = addToInputText("/")
+        }
+        button_multiply.setOnClickListener {
+            input.text = addToInputText("*")
+        }
+        button_subtraction.setOnClickListener {
+            input.text = addToInputText("-")
+        }
+        button_addition.setOnClickListener {
+            input.text = addToInputText("+")
+        }
+        button_equals.setOnClickListener {
+            showResult()
+        }
+
     }
+    private fun addToInputText(buttonValue: String): String {
+        return "${input.text} $buttonValue"
+    }
+
+    private fun getInputExpression(): String {
+        var expression = input.text.replace(Regex("/"),"/")
+        expression = expression.replace(Regex("x"), "*")
+        return expression
+    }
+
+    private fun showResult() {
+        try {
+            val expression = getInputExpression()
+            val result = Expression(expression).calculate()
+            if (result.isNon) {
+                //Show error message
+            }
+            else {
+                //Show result
+                output.text = DecimalFormat("0.######").format(result).toString()
+            }
+        }
+        catch (e: Exception) {
+                //Show error message
+        }
+    }
+
 }
